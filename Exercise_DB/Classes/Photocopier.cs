3c20d10a2662fea47cc818dc163c7952865251f2
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-07-17 10:11:17 +0200

    feat(Photocopier.cs): create photocopier class

---
+++ Exercise_DB/Classes/Photocopier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_DB.Classes
+{
+    internal class Photocopier
+    {
+        private int _id_photocopier;
+        private String _model;
+        private bool _is_failing;
+
+        static private int _id_count = 0;
+
+        public int Id_photocopier { get => _id_photocopier;}
+        public String Model { get => _model; set => _model = value; }
+        public bool Is_failing { get => _is_failing; set => _is_failing = value; }
+
+        public Photocopier(String model, bool is_failing)
+        {
+            _id_photocopier = _id_count;
+            Model = model;
+            Is_failing = is_failing;
+            
+            _id_count++;
+        }
+    }
+}
